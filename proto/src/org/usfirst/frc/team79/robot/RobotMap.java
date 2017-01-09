8ce61ba1d459eff78d754f6bb3cb4204e51cae91
ammhist <orionwolfe00@gmail.com>
2017-01-08 23:39:50 -0500

    Add files via upload

---
+++ proto/src/org/usfirst/frc/team79/robot/RobotMap.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team79.robot;
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+	//Joystick0
+	public static int drivestick = 1;
+	
+	//motors
+	public static int ForntLeftMotor = 1;
+	public static int BackLeftMotor = 3;
+	public static int FrontRightMotor = 0;
+	public static int BackRightMotor = 2;
+	public static int MiddleLeftMotor = 4;
+	public static int MiddleRightMotor = 5;
+	
+}
---
+++ proto/src/org/usfirst/frc/team79/robot/subsystems/DriveTrain.java
@@ -0,0 +1,48 @@
+package org.usfirst.frc.team79.robot.subsystems;
+
+import org.usfirst.frc.team79.robot.RobotMap;
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.CANTalon.TalonControlMode;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+
+public class DriveTrain extends Subsystem {
+    
+private RobotDrive Drive;
+public static CANTalon FrontLeft, BackLeft, FrontRight, BackRight , MiddleLeft, MiddleRight;
+
+	public DriveTrain(){
+		
+//		super();
+		FrontLeft = new CANTalon(RobotMap.ForntLeftMotor);
+		BackLeft = new CANTalon (RobotMap.BackRightMotor);
+	    FrontRight = new CANTalon (RobotMap.FrontRightMotor);
+	    BackRight = new CANTalon (RobotMap.BackRightMotor);
+	    MiddleRight = new CANTalon(RobotMap.MiddleRightMotor);
+	    MiddleLeft = new CANTalon(RobotMap.MiddleLeftMotor);
+	    
+	    Drive = new RobotDrive(FrontLeft, BackLeft, FrontRight, BackLeft);
+	    
+	}
+
+    public void initDefaultCommand() {
+    	
+    }
+    public void CANTalon() {
+     	MiddleLeft.changeControlMode(TalonControlMode.Follower);
+    	MiddleRight.changeControlMode(TalonControlMode.Follower);
+    	MiddleLeft.set(RobotMap.ForntLeftMotor);
+    	MiddleRight.set(RobotMap.FrontRightMotor);
+    }
+
+    public void arcadeDrive(Joystick stick){
+    	Drive.arcadeDrive(stick);
+    }
+    
+    public void arcadeDrive(double x, double y) {
+    	Drive.arcadeDrive(x, y);
+    }
+
+}
